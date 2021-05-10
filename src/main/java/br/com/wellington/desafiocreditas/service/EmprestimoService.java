8916e304ac29d25b444b760b21a1321eec632732
wdivino <wdivino@devires.com.br>
2021-05-10 18:50:59 -0300

    teste

--- src/main/java/br/com/wellington/desafiocreditas/service/EmprestimoService.java
+++ src/main/java/br/com/wellington/desafiocreditas/service/EmprestimoService.java
@@ -4,7 +4,7 @@ import br.com.wellington.desafiocreditas.model.Emprestimo;
 import br.com.wellington.desafiocreditas.model.EmprestimoEntradaDTO;
 
 import java.util.Set;
-
+ 
 public interface EmprestimoService {
     Set<Emprestimo> obterEmprestimosElegiveis(EmprestimoEntradaDTO emprestimoEntradaDTO);
 }
